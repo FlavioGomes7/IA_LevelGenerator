b508d06c09555aec26daa48ca1b1cff4ca837ea7
FlavioGomes7 <114361066+FlavioGomes7@users.noreply.github.com>
2023-09-15 22:15:25 -0300

    Init_comit

---
+++ IA_ProceduralLevels/Assets/Scripts/ExplorerAgent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+public class ExplorerAgent : Agent
+{
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        base.OnActionReceived(actions);
+    }
+
+}
